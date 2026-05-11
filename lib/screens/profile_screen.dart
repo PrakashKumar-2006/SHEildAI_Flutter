8da1cdee6263a313a157c0677f9e6214349f1105
Prakash Kumar Biswal <prakashkumarbiswal503@gmail.com>
2026-05-11 22:17:36 +0530

    Fix: resolve rogue period syntax error in profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,4 @@
-.import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/providers.dart';
 import '../features/auth/presentation/providers/auth_provider.dart';
